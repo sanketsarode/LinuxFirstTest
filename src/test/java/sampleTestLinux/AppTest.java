afd48a4d9d102c9bd8223654fb73a322508b7e34
Sanket Sarode <sanket.sarode41@gmail.com>
2019-02-21 21:56:41 +0000

    initial commit

---
+++ src/test/java/sampleTestLinux/AppTest.java
@@ -0,0 +1,33 @@
+package sampleTestLinux;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    private WebDriver driver;
+
+    @Test
+    public void testEasy() {
+        driver.get("http://demo.guru99.com/test/guru99home/");
+        String title = driver.getTitle();
+        Assert.assertTrue(title.contains("Demo Guru99 Page"));
+    }
+
+    @Before
+    public void beforeTest() {
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "//resources//drivers//chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void afterTest() {
+        driver.quit();
+    }
+}
