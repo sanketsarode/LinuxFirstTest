7ae289e25ec6eec4890aaed2810f3fb06dab3f18
Sanket Sarode <sanket.sarode41@gmail.com>
2019-02-22 10:30:28 +0000

    HTML UNIT ADDED

--- src/test/java/sampleTestLinux/AppTest.java
+++ src/test/java/sampleTestLinux/AppTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 /**
  * Unit test for simple App.
@@ -17,13 +18,14 @@ public class AppTest {
     public void testEasy() {
         driver.get("http://demo.guru99.com/test/guru99home/");
         String title = driver.getTitle();
+        System.out.println("****** " + driver.getTitle());
         Assert.assertTrue(title.contains("Demo Guru99 Page"));
     }
 
     @Before
     public void beforeTest() {
-        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "//resources//drivers//chromedriver");
-        driver = new ChromeDriver();
+        //System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "//resources//drivers//chromedriver");
+        driver = new HtmlUnitDriver();
     }
 
     @After
