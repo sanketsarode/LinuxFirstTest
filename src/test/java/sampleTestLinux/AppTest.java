2306c312a2b86f2348de8e50c0ffc450ee5a28b8
Sanket Sarode <sanket.sarode41@gmail.com>
2019-02-25 20:22:39 +0000

    Checkpoint added

--- src/test/java/sampleTestLinux/AppTest.java
+++ src/test/java/sampleTestLinux/AppTest.java
@@ -4,7 +4,9 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
@@ -19,6 +21,8 @@ public class AppTest {
         driver.get("http://demo.guru99.com/test/guru99home/");
         String title = driver.getTitle();
         System.out.println("****** " + driver.getTitle());
+        WebElement ele = driver.findElement(By.xpath("//h2[contains(text(),'All')]"));
+        System.out.println(ele.getText());
         Assert.assertTrue(title.contains("Demo Guru99 Page"));
     }
 
